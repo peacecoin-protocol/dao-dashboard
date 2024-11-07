50376e5ed6d71a51ca652858eaabb9c21e7eec72
pwollemi <daniel@snowball.money>
2024-11-08 01:16:23 +1000

    Added New ABIs and Contract Address

---
+++ src/app/ABIs/DAOStudio.ts
@@ -0,0 +1,127 @@
+export const DAO_STUDIO_ABI = [
+  {
+    type: 'function',
+    name: 'create',
+    inputs: [
+      { name: '_communityToken', type: 'address', internalType: 'address' },
+    ],
+    outputs: [],
+    stateMutability: 'nonpayable',
+  },
+  {
+    type: 'function',
+    name: 'daos',
+    inputs: [{ name: '', type: 'address', internalType: 'address' }],
+    outputs: [
+      { name: 'isCreated', type: 'bool', internalType: 'bool' },
+      { name: 'governor', type: 'address', internalType: 'address' },
+      { name: 'timelock', type: 'address', internalType: 'address' },
+    ],
+    stateMutability: 'view',
+  },
+  {
+    type: 'function',
+    name: 'deleteDAO',
+    inputs: [
+      { name: '_communityToken', type: 'address', internalType: 'address' },
+    ],
+    outputs: [],
+    stateMutability: 'nonpayable',
+  },
+  {
+    type: 'function',
+    name: 'initialize',
+    inputs: [
+      { name: '_owner', type: 'address', internalType: 'address' },
+      { name: '_pceToken', type: 'address', internalType: 'address' },
+    ],
+    outputs: [],
+    stateMutability: 'nonpayable',
+  },
+  {
+    type: 'function',
+    name: 'owner',
+    inputs: [],
+    outputs: [{ name: '', type: 'address', internalType: 'address' }],
+    stateMutability: 'view',
+  },
+  {
+    type: 'function',
+    name: 'pceToken',
+    inputs: [],
+    outputs: [{ name: '', type: 'address', internalType: 'address' }],
+    stateMutability: 'view',
+  },
+  {
+    type: 'function',
+    name: 'renounceOwnership',
+    inputs: [],
+    outputs: [],
+    stateMutability: 'nonpayable',
+  },
+  {
+    type: 'function',
+    name: 'setTimelockDelay',
+    inputs: [{ name: '_delay', type: 'uint256', internalType: 'uint256' }],
+    outputs: [],
+    stateMutability: 'nonpayable',
+  },
+  {
+    type: 'function',
+    name: 'timelockDelay',
+    inputs: [],
+    outputs: [{ name: '', type: 'uint256', internalType: 'uint256' }],
+    stateMutability: 'view',
+  },
+  {
+    type: 'function',
+    name: 'transferOwnership',
+    inputs: [{ name: 'newOwner', type: 'address', internalType: 'address' }],
+    outputs: [],
+    stateMutability: 'nonpayable',
+  },
+  {
+    type: 'event',
+    name: 'Initialized',
+    inputs: [
+      {
+        name: 'version',
+        type: 'uint64',
+        indexed: false,
+        internalType: 'uint64',
+      },
+    ],
+    anonymous: false,
+  },
+  {
+    type: 'event',
+    name: 'OwnershipTransferred',
+    inputs: [
+      {
+        name: 'previousOwner',
+        type: 'address',
+        indexed: true,
+        internalType: 'address',
+      },
+      {
+        name: 'newOwner',
+        type: 'address',
+        indexed: true,
+        internalType: 'address',
+      },
+    ],
+    anonymous: false,
+  },
+  { type: 'error', name: 'InvalidInitialization', inputs: [] },
+  { type: 'error', name: 'NotInitializing', inputs: [] },
+  {
+    type: 'error',
+    name: 'OwnableInvalidOwner',
+    inputs: [{ name: 'owner', type: 'address', internalType: 'address' }],
+  },
+  {
+    type: 'error',
+    name: 'OwnableUnauthorizedAccount',
+    inputs: [{ name: 'account', type: 'address', internalType: 'address' }],
+  },
+]
--- src/app/constants/constants.tsx
+++ src/app/constants/constants.tsx
@@ -7,6 +7,7 @@ export const pceCommunity = '0xf939595726798393F63Dbe098a54C7948DEF8faF'
 export const bountyAddress = '0x4C0195cE264457549e97f84475bcA5bf72e63db8'
 export const factoryAddress = '0x98120e33beea75f4dafe4ac249b3b2f16586f5a2'
 export const POLY_SCAN_TX = 'https://amoy.polygonscan.com/tx/'
+export const dao_studioAddress = '0x1b08Ea388EE4AED84ad69A7C576ca0b8398baaA7'
 export const provider = new ethers.JsonRpcProvider(
   'https://polygon-amoy.blockpi.network/v1/rpc/public'
 )
