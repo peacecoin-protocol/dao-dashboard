95f6dd8257383dc1ced9270e948138fb5712e85e
pwollemi <smartcodefan@gmail.com>
2025-06-19 21:14:21 -0400

    removed unused variants

--- src/components/custom/delegate-input.tsx
+++ src/components/custom/delegate-input.tsx
@@ -11,40 +11,9 @@ import {
 } from '~/components/ui/dialog'
 import { Input } from '~/components/ui/input'
 import { cn } from '~/components/utils'
-import { type VariantProps, cva } from 'class-variance-authority'
-
-const delegateInputVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
-  {
-    variants: {
-      variant: {
-        default: 'bg-primary text-primary-foreground hover:bg-primary/90',
-        destructive:
-          'bg-destructive text-destructive-foreground hover:bg-destructive/90',
-        outline:
-          'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
-        secondary:
-          'bg-secondary text-secondary-foreground hover:bg-secondary/80',
-        ghost: 'hover:bg-accent hover:text-accent-foreground',
-        link: 'text-primary underline-offset-4 hover:underline',
-      },
-      size: {
-        default: 'h-10 px-4 py-2',
-        sm: 'h-9 rounded-md px-3',
-        lg: 'h-11 rounded-md px-8',
-        icon: 'h-10 w-10',
-      },
-    },
-    defaultVariants: {
-      variant: 'default',
-      size: 'default',
-    },
-  }
-)
 
 export interface DelegateInputProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof delegateInputVariants> {
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   setDelegateAddr: (amount: string) => void
   handleDelegate: () => void
   asChild?: boolean
@@ -52,21 +21,16 @@ export interface DelegateInputProps
 
 const DelegateInput = React.forwardRef<HTMLInputElement, DelegateInputProps>(
   (
-    {
-      setDelegateAddr,
-      handleDelegate,
-      className,
-      variant,
-      size,
-      asChild = false,
-      ...props
-    },
+    { setDelegateAddr, handleDelegate, className, asChild = false, ...props },
     ref
   ) => {
     return (
       <Dialog>
         <DialogTrigger
-          className={cn(delegateInputVariants({ variant, size, className }))}
+          className={cn(
+            'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2',
+            className
+          )}
         >
           Delegate
         </DialogTrigger>
--- src/components/custom/dialog-github.tsx
+++ src/components/custom/dialog-github.tsx
@@ -8,42 +8,11 @@ import {
   DialogTitle,
 } from '~/components/ui/dialog'
 
-import { type VariantProps, cva } from 'class-variance-authority'
 import { PIP } from '~/i18n/types'
 import ReactMarkdown from 'react-markdown'
 
-const dialogInputVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
-  {
-    variants: {
-      variant: {
-        default: 'bg-primary text-primary-foreground hover:bg-primary/90',
-        destructive:
-          'bg-destructive text-destructive-foreground hover:bg-destructive/90',
-        outline:
-          'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
-        secondary:
-          'bg-secondary text-secondary-foreground hover:bg-secondary/80',
-        ghost: 'hover:bg-accent hover:text-accent-foreground',
-        link: 'text-primary underline-offset-4 hover:underline',
-      },
-      size: {
-        default: 'h-10 px-4 py-2',
-        sm: 'h-9 rounded-md px-3',
-        lg: 'h-11 rounded-md px-8',
-        icon: 'h-10 w-10',
-      },
-    },
-    defaultVariants: {
-      variant: 'default',
-      size: 'default',
-    },
-  }
-)
-
 export interface DialogGithubProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof dialogInputVariants> {
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   open: boolean
   localDict: any
   asChild?: boolean
@@ -53,17 +22,7 @@ export interface DialogGithubProps
 
 const DialogGithub = React.forwardRef<HTMLInputElement, DialogGithubProps>(
   (
-    {
-      className,
-      variant,
-      size,
-      localDict,
-      open,
-      asChild = false,
-      pip,
-      setOpen,
-      ...props
-    },
+    { className, localDict, open, asChild = false, pip, setOpen, ...props },
     ref
   ) => {
     return (
@@ -73,12 +32,6 @@ const DialogGithub = React.forwardRef<HTMLInputElement, DialogGithubProps>(
             <DialogTitle>{pip?.title || ''}</DialogTitle>
             <DialogDescription className="flex flex-col gap-2 overflow-y-auto overflow-x-auto max-h-[60vh]">
               <div className="flex flex-row gap-2">
-                {/* <Image
-                  src={pip?.proposer || ''}
-                  alt={pip?.proposer || ''}
-                  width={48}
-                  height={48}
-                /> */}
                 <div className="flex flex-col gap-1">
                   <p>{pip?.proposer || ''}</p>
                   <p>{pip?.created || ''}</p>
